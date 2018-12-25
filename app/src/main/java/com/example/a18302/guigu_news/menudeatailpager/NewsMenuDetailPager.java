1462852f68bb60aa1ee1d84067cb717654d45a23
yht110 <183023376@qq.com>
2018-12-25 22:56:35 +0800

    页签页面的布局和联网请求

--- app/src/main/java/com/example/a18302/guigu_news/menudeatailpager/NewsMenuDetailPager.java
+++ app/src/main/java/com/example/a18302/guigu_news/menudeatailpager/NewsMenuDetailPager.java
@@ -1,21 +1,21 @@
 package com.example.a18302.guigu_news.menudeatailpager;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
+import com.example.a18302.guigu_news.MainActivity;
 import com.example.a18302.guigu_news.R;
 import com.example.a18302.guigu_news.base.MenuDetailBasePager;
-import com.example.a18302.guigu_news.domain.NewsCenterPagerBean;
 import com.example.a18302.guigu_news.domain.NewsCenterPagerBean2;
 import com.example.a18302.guigu_news.menudeatailpager.tabdetailpager.TabDetailPager;
 import com.example.a18302.guigu_news.utils.LogUtil;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.viewpagerindicator.TabPageIndicator;
 
 import org.xutils.view.annotation.ViewInject;
@@ -26,11 +26,16 @@ import java.util.List;
 
 public class NewsMenuDetailPager extends MenuDetailBasePager {
 
+
+
     @ViewInject(R.id.tabPageIndicator)
     private TabPageIndicator tabPageIndicator;
 
     @ViewInject(R.id.viewpager)
     private ViewPager viewPager;
+
+    @ViewInject(R.id.ib_tab_next)
+    private ImageButton ib_tab_next;
 
     //页签页面的数据集合
     private List<NewsCenterPagerBean2.DetailPagerData.ChildrenData> children;
@@ -48,6 +53,12 @@ public class NewsMenuDetailPager extends MenuDetailBasePager {
     protected View initView() {
         View view = View.inflate(context, R.layout.newsmenu_detail_pager, null);
         x.view().inject(NewsMenuDetailPager.this, view);
+        ib_tab_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+            }
+        });
         return view;
     }
 
@@ -60,10 +71,51 @@ public class NewsMenuDetailPager extends MenuDetailBasePager {
         for (int i = 0; i < children.size(); i++) {
             tabDetailPagers.add(new TabDetailPager(context, children.get(i)));
         }
+        //设置viewpager的适配器
         viewPager.setAdapter(new MyNewMenuDetailPagerAdapter());
+        //viewpager和tabPagerIndiacator的关联
+        tabPageIndicator.setViewPager(viewPager);
+
+        //注意监听页面的变化，TabPageIndicator监听页面变化
+        tabPageIndicator.setOnPageChangeListener(new MyOnChangeListener());
+    }
+
+
+    private void isEnableSlideMenu(int touchmodeFullScreen) {
+        MainActivity mainActivity = (MainActivity) context;
+        mainActivity.getSlidingMenu().setTouchModeAbove(touchmodeFullScreen);
+    }
+
+    class MyOnChangeListener implements ViewPager.OnPageChangeListener {
+
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+            if (i == 0) {
+                //SlideMenu可以全屏滑动
+                isEnableSlideMenu(SlidingMenu.TOUCHMODE_FULLSCREEN);
+            } else {
+                isEnableSlideMenu(SlidingMenu.TOUCHMODE_NONE);
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
     }
 
     class MyNewMenuDetailPagerAdapter extends PagerAdapter {
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return children.get(position).getTitle();
+        }
 
         @NonNull
         @Override
--- app/src/main/java/com/example/a18302/guigu_news/menudeatailpager/tabdetailpager/TabDetailPager.java
+++ app/src/main/java/com/example/a18302/guigu_news/menudeatailpager/tabdetailpager/TabDetailPager.java
@@ -2,20 +2,32 @@ package com.example.a18302.guigu_news.menudeatailpager.tabdetailpager;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.a18302.guigu_news.R;
 import com.example.a18302.guigu_news.base.MenuDetailBasePager;
 import com.example.a18302.guigu_news.domain.NewsCenterPagerBean;
 import com.example.a18302.guigu_news.domain.NewsCenterPagerBean2;
+import com.example.a18302.guigu_news.domain.TabDetailPagerBean;
+import com.example.a18302.guigu_news.utils.CacheUtils;
+import com.example.a18302.guigu_news.utils.Contants;
+import com.google.gson.Gson;
+
+import org.xutils.common.Callback;
+import org.xutils.common.util.LogUtil;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
 
 /**
  * 页签页面
  */
 public class TabDetailPager extends MenuDetailBasePager {
     private final NewsCenterPagerBean2.DetailPagerData.ChildrenData childrenData;
-    private TextView textView;
+
+    private String url;
 
     public TabDetailPager(Context context, NewsCenterPagerBean2.DetailPagerData.ChildrenData childrenData) {
         super(context);
@@ -24,16 +36,57 @@ public class TabDetailPager extends MenuDetailBasePager {
 
     @Override
     protected View initView() {
-        textView = new TextView(context);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        textView.setTextSize(25);
-        return textView;
+        View view = View.inflate(context, R.layout.tabdetail_pager, null);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText(childrenData.getTitle());
+        url = Contants.BASE_URL + childrenData.getUrl();
+        //把之前的缓存数据取出
+        String saveJson = CacheUtils.getString(context,url);
+        if (!TextUtils.isEmpty(saveJson)) {
+            //解析和显示处理的数据
+            ProcessData(saveJson);
+        }
+        //联网请求数据
+        getDataFromNet();
+    }
+
+    private void ProcessData(String saveJson) {
+        TabDetailPagerBean bean = parseJson(saveJson);
+        LogUtil.e(childrenData.getTitle() + "解析成功" + bean.getData().getNews().get(0).getTitle());
+    }
+
+    private TabDetailPagerBean parseJson(String saveJson) {
+        return new Gson().fromJson(saveJson,TabDetailPagerBean.class);
+
+    }
+
+    private void getDataFromNet() {
+        RequestParams params = new RequestParams(url);
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                CacheUtils.putString(context,url,result);
+                LogUtil.e(childrenData.getTitle() + "-页面数据请求成功==" + result);
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                LogUtil.e(childrenData.getTitle() + "-页面数据请求失败==" + ex.getMessage());
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+                LogUtil.e(childrenData.getTitle() + "-页面数据请求取消==" + cex.getMessage());
+            }
+
+            @Override
+            public void onFinished() {
+                LogUtil.e(childrenData.getTitle() + "-页面数据请求完成");
+            }
+        });
     }
 }

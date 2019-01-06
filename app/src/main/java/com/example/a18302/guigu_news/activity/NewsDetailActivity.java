6aeb9068cb5797a75b93c80b18023c9659e64463
yht110 <183023376@qq.com>
2019-01-06 13:43:13 +0800

    webview新闻详情页字体大小的设置

--- app/src/main/java/com/example/a18302/guigu_news/activity/NewsDetailActivity.java
+++ app/src/main/java/com/example/a18302/guigu_news/activity/NewsDetailActivity.java
@@ -1,5 +1,7 @@
 package com.example.a18302.guigu_news.activity;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,6 +27,8 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
     private ProgressBar pbLoading;
 
     String url;
+
+    private WebSettings webSettings;
 
     /**
      * Find the Views in the layout<br />
@@ -66,9 +70,58 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
             finish();
             // 文字大小
         } else if (v == ibTextsize) {
+            showChangeTextSizeDialog();
+
             // 分享
         } else if (v == ibShare) {
 
+        }
+    }
+
+    private int tempSize = 2;
+    private int realSize = tempSize;
+
+    /**
+     * 文字大小设置的改变
+     */
+    private void showChangeTextSizeDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("设置文字大小");
+        String[] items = {"超大字体","大字体","正常字体","小字体","超小字体"};
+        builder.setSingleChoiceItems(items, realSize, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                tempSize = i;
+            }
+        });
+        builder.setNegativeButton("取消",null);
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                realSize = tempSize;
+                changeTextSize(realSize);
+            }
+        });
+        builder.show();
+    }
+
+    private void changeTextSize(int realSize) {
+        switch (realSize) {
+            case 0:
+                webSettings.setTextZoom(200);
+                break;
+            case 1:
+                webSettings.setTextZoom(150);
+                break;
+            case 2:
+                webSettings.setTextZoom(100);
+                break;
+            case 3:
+                webSettings.setTextZoom(75);
+                break;
+            case 4:
+                webSettings.setTextZoom(50);
+                break;
         }
     }
 
@@ -83,7 +136,7 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
 
     public void getData() {
         url = getIntent().getStringExtra("url");
-        WebSettings webSettings = webview.getSettings();
+        webSettings = webview.getSettings();
         //设置支持javaScript
         webSettings.setJavaScriptEnabled(true);
         //设置双击变大变小
@@ -91,8 +144,11 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
         //增加缩放按钮
         webSettings.setBuiltInZoomControls(true);
 
+        //设置文字大小
+//        webSettings.setTextSize(WebSettings.TextSize.NORMAL);
+        webSettings.setTextZoom(100);
         //不让当前的网页跳转到系统的浏览器
-        webview.setWebViewClient(new WebViewClient(){
+        webview.setWebViewClient(new WebViewClient() {
             //加载页面完成的时候回调
             @Override
             public void onPageFinished(WebView view, String url) {
